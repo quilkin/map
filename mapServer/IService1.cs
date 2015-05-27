db51121135b53ec5ceb81f823215d03e084f4c74
quilkin <quilkin2@gmail.com>
2015-05-27 21:46:59 +0100

    map2 - iservice test

--- mapServer/IService1.cs
+++ mapServer/IService1.cs
@@ -31,22 +31,6 @@ namespace BleLog
         [WebInvoke(Method = "POST", UriTemplate = "/Signup", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
         string Signup(Login login);
 
-        //[OperationContract]
-        //[WebInvoke(Method = "POST", UriTemplate = "/SaveEvent", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
-        //string SaveEvent(Event ev);
-
-        //[OperationContract]
-        //[WebInvoke(Method = "POST", UriTemplate = "/LoadEvents", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
-        //IEnumerable<Event> LoadEvents(Event ev);
-
-        //[OperationContract]
-        //[WebInvoke(Method = "POST", UriTemplate = "/LoadEntries", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
-        //IEnumerable<Entry> LoadEntries(int eventID);
-
-        //[OperationContract]
-        //[WebInvoke(Method = "POST", UriTemplate = "/SeedEntries", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
-        //IEnumerable<Entry> SeedEntries(Event ev);
-
         [OperationContract]
         [WebInvoke(Method = "POST", UriTemplate = "/SaveLogdata", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
         UploadResult SaveLogdata(IEnumerable<Logdata> logdata);
