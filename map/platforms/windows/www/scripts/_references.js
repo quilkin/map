57e157e356758b6fb19e18b514e24c4b9c70d45c
quilkin <quilkin2@gmail.com>
2016-06-20 20:06:06 +0100

    routing

--- map/platforms/windows/www/scripts/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="myMap.js" />
-/// <reference path="opencyclemap.js" />
-/// <reference path="index.js" />
-/// <reference path="jquery-1.11.1.min.js" />
-/// <reference path="~\openlayers\OpenLayers.js" />
-/// <reference path="data.js" />
